fb585811ab72f4e279ff0851e3316279155d0fce
Till Mahlburg <mahlburg@posteo.de>
2022-07-22 14:39:37 +0200

    more initial prototyping

--- mriscv.srcs/sources_1/new/instruction_decode.v
+++ mriscv.srcs/sources_1/new/instruction_decode.v
@@ -25,39 +25,51 @@ module instruction_decode (
 
 	input [31:0] instr,
 
-	output [31:0] operator,
-	output [31:0] operand_a,
-	output [31:0] operand_b,
-	output [4:0] dest
+	output reg [31:0] operator,
+	output reg is_mem,
+	output reg [31:0] operand_a,
+	output reg [31:0] operand_b,
+	output reg [4:0] dest
 );
-	reg [3:0] type = 0;
-
 	always @(posedge clk) begin
 		operator = instr[6:0];
+		is_mem = 1'b0;
 		case (instr[6:0])
 			/* R-type: register-register */
-			7'b0110011: type = 1;
+			7'b0110011: begin
+				dest = instr[11:7];
+				/* operand_a_reg = instr[19:15] */
+				/* operand_b_reg = instr[24:20] */
+			end
 			/* I-type: short immediates, loads */
-			7'b1100111 || 7'b0000011 || 7'b0010011 || 7'b0001111 || 7'b1110011: type = 2;
+			7'b1100111 || 7'b0000011 || 7'b0010011 || 7'b0001111 || 7'b1110011: begin
+				if (instr[6:0] == 7'b0000011) begin
+					is_mem = 1'b1;
+				end
+				dest = instr[11:7];
+				/* operand_a_reg  = instr[19:15] */
+				operand_b = {{20{instr[31]}}, instr[31:20]};
+			end
 			/* S-type: stores */
-			7'b0100011: type = 3;
+			7'b0100011: begin
+				is_mem = 1'b1;
+				/* operand_a_reg = instr[19:15] /* + immediate */
+				/* operand_b_reg = instr[24:19] */
+			end
 			/* B-type: conditional branches */
-			7'b1100011: type = 4;
+			7'b1100011: ; /* TODO */
 			/* U-type: long immediates */
 			7'b0110111 || 7'b0010111: begin
 				dest = instr[11:7];
-				operand_a[31:12] = instr[31:12];
-				operand_a[11:0] = 0;
+				operand_a = {instr[31:12], 12'b000000000000};
 			end
 			/* J-type: unconditional jumps */
 			7'b1101111: begin
 				dest = instr[11:7];
-				operand_a[31:21] = 0;
-				operand_a[20] = instr[31];
-				operand_a[19:12] = instr[19:12];
-				operand_a[11] = instr[20];
-				operand_a[10:1] = instr[30:21];
+
+				operand_a = {11'b00000000000, instr[31], instr[19:12], instr[20], instr[30:21]}
 			end
+			default: ;
 		endcase
 	end
 
