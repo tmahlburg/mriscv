12f923408051c8e583a6d776950a06177499c67c
Till Mahlburg <mahlburg@posteo.de>
2022-07-25 19:26:54 +0200

    further prototyping

--- mriscv.srcs/sources_1/new/instruction_decode.v
+++ mriscv.srcs/sources_1/new/instruction_decode.v
@@ -25,8 +25,6 @@ module instruction_decode (
 
 	input [31:0] instr,
 
-	output reg [31:0] operator,
-
 	output reg is_store,
 	output reg is_load,
 
@@ -59,28 +57,36 @@ module instruction_decode (
 	assign func3 = instr[14:12];
 	assign func7 = instr[30];
 
+	assign dest = instr[11:7];
+
 	/* instr decode */
 	always @(posedge clk) begin
 		operator = instr[6:0];
 		is_store = 1'b0;
 		is_load = 1'b0;
 		is_branch = 1'b0;
+		is_alu = 1'b0;
 
-		case (instr[6:0])
+		case (instr[6:0]) begin
 			/* R-type: register-register */
 			7'b0110011: begin
-				dest = instr[11:7];
 				operand_a = rs1;
 				operand_b = rs2;
+				is_alu = 1'b1;
 			end
 			/* I-type: short immediates, loads */
 			7'b1100111 || 7'b0000011 || 7'b0010011 || 7'b0001111 || 7'b1110011: begin
-				if (instr[6:0] == 7'b0000011) begin
-					is_load = 1'b1;
-				end else if (instr[6:0] == 7'b1100111) begin
-					is_branch = 1'b1;
-				end
-				dest = instr[11:7];
+				case (instr[6:0]) begin
+					7'b0000011: begin
+						is_load = 1'b1;
+					end
+					7'b1100111: begin
+						is_branch = 1'b1;
+					end
+					7'b0010011: begin
+						is_alu = 1'b1;
+					end
+				endcase
 				operand_a = rs1;
 				operand_b = {{20{instr[31]}}, instr[31:20]};
 			end
@@ -96,17 +102,14 @@ module instruction_decode (
 			end
 			/* U-type: long immediates */
 			7'b0110111 || 7'b0010111: begin
-				dest = instr[11:7];
 				operand_a = {instr[31:12], 12'b000000000000};
 				is_branch = 1'b1;
 			end
 			/* J-type: unconditional jumps */
 			7'b1101111: begin
-				dest = instr[11:7];
-
 				operand_a = {11'b00000000000, instr[31], instr[19:12], instr[20], instr[30:21]}
 			end
-			default: ;
+			default: ; /* unknown opcode */
 		endcase
 	end
 endmodule
