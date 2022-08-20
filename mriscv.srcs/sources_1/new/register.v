5f3b78a0bb5dd64d9d6bafcb7b15b6e7b9158d13
Till Mahlburg <mahlburg@posteo.de>
2022-08-20 19:23:40 +0200

    add simple register implementation

---
+++ mriscv.srcs/sources_1/new/register.v
@@ -0,0 +1,14 @@
+`timescale 1ns / 1ps
+
+module regs (
+	input [4:0] raddr1,
+	input [4:0] raddr2,
+
+	output reg [31:0] rdata1,
+	output reg [31:0] rdata2
+);
+	reg [31:0] register [0:31];
+
+	assign rdata1 = register[raddr1];
+	assign rdata2 = register[raddr2];
+endmodule
