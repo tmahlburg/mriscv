43370061ccb7ac8d40f4a89a75a5d63b24a8cb1d
Till Mahlburg <mahlburg@posteo.de>
2022-08-20 19:24:04 +0200

    add shift

--- mriscv.srcs/sources_1/new/execute.v
+++ mriscv.srcs/sources_1/new/execute.v
@@ -7,7 +7,6 @@ module execute (
 	input is_load,
 	input is_branch,
 	input is_alu,
-	input is_shift,
 
 	input [31:0] operand_a,
 	input [31:0] operand_b,
@@ -21,6 +20,8 @@ module execute (
 )
 
 	always @(posedge clk) begin
+		is_pc = 1'b0;
+
 		case 1'b1 begin
 			is_store: ; /* TODO */
 
@@ -28,7 +29,7 @@ module execute (
 
 			is_branch: ; /* TODO */
 
-			is_alu:
+			is_alu: begin
 				case func3
 					/* add */
 					3'b000: begin
@@ -50,12 +51,22 @@ module execute (
 					3'b100: begin
 						result = operand_a ^ operand_b;
 					end
+					/* left shift */
+					3'b001: begin
+						result = operand_a << operand_b[4:0];
+					end
+					/* right shift */
+					3'b101: begin
+						/* logical */
+						if (func7 === 1'b0) begin
+							result = operand_a >> operand_b[4:0];
+						/* arithmetic */
+						end else begin
+							result = operand_a >>> operand_b[4:0];
+						end
+					end
 				endcase
-
-			is_shift:
-				case func3:
-
-				endcase
+			end
 		endcase
 	end
 
