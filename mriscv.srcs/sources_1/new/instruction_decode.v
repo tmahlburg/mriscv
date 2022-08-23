1c5a0ef4f327e760625676b928026fe0afba7400
Till Mahlburg <mahlburg@posteo.de>
2022-08-23 13:25:21 +0200

    differentiate jumps

--- mriscv.srcs/sources_1/new/instruction_decode.v
+++ mriscv.srcs/sources_1/new/instruction_decode.v
@@ -21,7 +21,7 @@
 
 
 module instruction_decode (
-	input clk, resetn,
+	input clk, reset,
 
 	input [31:0] instr,
 
@@ -30,11 +30,13 @@ module instruction_decode (
 
 	output reg is_branch,
 	output reg is_jump,
+	output reg is_link,
+	output reg is_rel,
 
 	output reg is_alu,
 	output reg [31:0] operand_a,
 	output reg [31:0] operand_b,
-	output reg [4:0] dest,
+	output [4:0] dest,
 
 	output [2:0] func3,
 	output func7,
@@ -61,68 +63,78 @@ module instruction_decode (
 
 	/* instr decode */
 	always @(posedge clk) begin
-		operator = instr[6:0];
-		is_store = 1'b0;
-		is_load = 1'b0;
-		is_branch = 1'b0;
-		is_jump = 1'b0;
-		is_alu = 1'b0;
+		if (!reset) begin
+			operator = instr[6:0];
+			is_store = 1'b0;
+			is_load = 1'b0;
 
-		case (instr[6:0]) begin
-			/* R-type: register-register */
-			7'b0110011: begin
-				operand_a = rs1;
-				operand_b = rs2;
-				is_alu = 1'b1;
-			end
-			/* I-type: short immediates, loads */
-			7'b1100111 || 7'b0000011 || 7'b0010011 || 7'b0001111 || 7'b1110011: begin
-				operand_a = rs1;
-				operand_b = {{20{instr[31]}}, instr[31:20]};
-				case (instr[6:0]) begin
-					7'b0000011: begin
-						is_load = 1'b1;
-					end
-					/* jalr */
-					7'b1100111: begin
-						is_jump = 1'b1;
-					end
-					7'b0010011: begin
-						is_alu = 1'b1;
-						/* is shift operation? operand_b = shamt */
-						if (func3 == 3'b001 || func3 == 3'b101) begin
-							operand_b = instr[24:20];
+			is_branch = 1'b0;
+			is_jump = 1'b0;
+			is_link = 1'b0;
+			is_rel = 1'b0;
+
+			is_alu = 1'b0;
+
+			case (instr[6:0]) begin
+				/* R-type: register-register */
+				7'b0110011: begin
+					operand_a = rs1;
+					operand_b = rs2;
+					is_alu = 1'b1;
+				end
+				/* I-type: short immediates, loads */
+				7'b1100111 || 7'b0000011 || 7'b0010011 || 7'b0001111 || 7'b1110011: begin
+					operand_a = rs1;
+					operand_b = {{20{instr[31]}}, instr[31:20]};
+					case (instr[6:0]) begin
+						7'b0000011: begin
+							is_load = 1'b1;
 						end
-					end
-				endcase
-			end
-			/* S-type: stores */
-			7'b0100011: begin
-				is_store = 1'b1;
-				operand_a = instr[19:15] + {{20{instr[31]}}, instr[31:25], instr[11:7]};
-				operand_b = rs2;
-			end
-			/* B-type: conditional branches */
-			7'b1100011: begin
-				is_branch = 1'b1;
-			end
-			/* U-type: long immediates */
-			7'b0110111 || 7'b0010111: begin
-				operand_a = {instr[31:12], 12'b000000000000};
-				case (instr[6:0]) begin
-					/* lui */
-					7'b0110111:
-						is_load = 1'b1;
-					7'b0010111:
-						is_jump = 1'b1;
-				endcase
-			end
-			/* J-type: unconditional jumps */
-			7'b1101111: begin
-				operand_a = {11'b00000000000, instr[31], instr[19:12], instr[20], instr[30:21]}
-				is_jump = 1'b1;
-			end
-			default: ; /* unknown opcode */
-		endcase
+						/* jalr */
+						7'b1100111: begin
+							is_jump = 1'b1;
+							is_rel = 1'b1;
+							is_link = 1'b1;
+						end
+						7'b0010011: begin
+							is_alu = 1'b1;
+							/* is shift operation? operand_b = shamt */
+							if (func3 == 3'b001 || func3 == 3'b101) begin
+								operand_b = instr[24:20];
+							end
+						end
+					endcase
+				end
+				/* S-type: stores */
+				7'b0100011: begin
+					is_store = 1'b1;
+					operand_a = instr[19:15] + {{20{instr[31]}}, instr[31:25], instr[11:7]};
+					operand_b = rs2;
+				end
+				/* B-type: conditional branches */
+				7'b1100011: begin
+					is_branch = 1'b1;
+				end
+				/* U-type: long immediates */
+				7'b0110111 || 7'b0010111: begin
+					operand_a = {instr[31:12], 12'b000000000000};
+					case (instr[6:0]) begin
+						/* lui */
+						7'b0110111:
+							is_load = 1'b1;
+						/* aupic */
+						7'b0010111:
+							is_jump = 1'b1;
+					endcase
+				end
+				/* J-type: unconditional jumps */
+				7'b1101111: begin
+					operand_a = {11'b00000000000, instr[31], instr[19:12], instr[20], instr[30:21]}
+					is_jump = 1'b1;
+					is_link = 1'b1;
+				end
+				default: ; /* unknown opcode */
+			endcase
+		end
 	end
 endmodule
