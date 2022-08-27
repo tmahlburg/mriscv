6e89ba4831f610c306806e4f9c31a17dac696095
Till Mahlburg <mahlburg@posteo.de>
2022-08-27 12:32:21 +0200

    use correct assignment for sequential logic (<=)

--- mriscv.srcs/sources_1/new/execute.v
+++ mriscv.srcs/sources_1/new/execute.v
@@ -30,10 +30,10 @@ module execute (
 	reg branch;
 
 	always @(posedge clk) begin
-		dest_o = dest_i;
-		branch = 1'b0;
+		dest_o <= dest_i;
+		branch <= 1'b0;
 
-		next_pc = curr_pc + 4;
+		next_pc <= curr_pc + 4;
 
 		case (1'b1)
 			is_store: ; /* TODO */
@@ -43,32 +43,32 @@ module execute (
 			is_branch: begin
 				case (func3)
 					/* beq */
-					3'b000: branch = (operand_a == operand_b);
+					3'b000: branch <= (operand_a == operand_b);
 					/* bne */
-					3'b001: branch = (operand_a != operand_b);
+					3'b001: branch <= (operand_a != operand_b);
 					/* blt */
-					3'b100: branch = (operand_a < operand_b);
+					3'b100: branch <= (operand_a < operand_b);
 					/* bge */
-					3'b101: branch = (operand_a >= operand_b);
+					3'b101: branch <= (operand_a >= operand_b);
 					/* bltu */
-					3'b110: branch = ($unsigned(operand_a) < $unsigned(operand_b));
+					3'b110: branch <= ($unsigned(operand_a) < $unsigned(operand_b));
 					/* bgeu */
-					3'b111: branch = ($unsigned(operand_a) >= $unsigned(operand_b));
+					3'b111: branch <= ($unsigned(operand_a) >= $unsigned(operand_b));
 				endcase
 				if (branch) begin
-					next_pc = curr_pc + branch_dest;
+					next_pc <= curr_pc + branch_dest;
 				end
 			end
 			is_jump: begin
 				result = curr_pc + 4;
 				/* when there is no destination specified, register x1 is assumed */
 				if (dest_i == 5'b00000) begin
-					dest_o = 5'b00001;
+					dest_o <= 5'b00001;
 				end
 				if (is_reg) begin
-					next_pc = (operand_a + operand_b) & ~1;
+					next_pc <= (operand_a + operand_b) & ~1;
 				end else begin
-					next_pc = curr_pc + operand_a;
+					next_pc <= curr_pc + operand_a;
 				end
 			end
 
@@ -77,35 +77,35 @@ module execute (
 					/* add */
 					3'b000: begin
 						if (func7 === 1'b0) begin
-							result = operand_a + operand_b;
+							result <= operand_a + operand_b;
 						end else begin
-							result = operand_a - operand_b;
+							result <= operand_a - operand_b;
 						end
 					end
 					/* and */
 					3'b111: begin
-						result = operand_a & operand_b;
+						result <= operand_a & operand_b;
 					end
 					/* or */
 					3'b110: begin
-						result = operand_a | operand_b;
+						result <= operand_a | operand_b;
 					end
 					/* xor */
 					3'b100: begin
-						result = operand_a ^ operand_b;
+						result <= operand_a ^ operand_b;
 					end
 					/* left shift */
 					3'b001: begin
-						result = operand_a << operand_b[4:0];
+						result <= operand_a << operand_b[4:0];
 					end
 					/* right shift */
 					3'b101: begin
 						/* logical */
 						if (func7 === 1'b0) begin
-							result = operand_a >> operand_b[4:0];
+							result <= operand_a >> operand_b[4:0];
 						/* arithmetic */
 						end else begin
-							result = operand_a >>> operand_b[4:0];
+							result <= operand_a >>> operand_b[4:0];
 						end
 					end
 				endcase
--- mriscv.srcs/sources_1/new/instr_decode.v
+++ mriscv.srcs/sources_1/new/instr_decode.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
-// Create Date: 21.07.2022 11:45:49
-// Design Name:
-// Module Name: instruction_decode
-// Project Name:
-// Target Devices:
-// Tool Versions:
-// Description:
-//
-// Dependencies:
-//
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-//
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module instr_decode (
@@ -65,82 +46,82 @@ module instr_decode (
 	/* instr decode */
 	always @(posedge clk) begin
 		if (reset) begin
-			is_store = 1'b0;
-			is_load = 1'b0;
-			is_branch = 1'b0;
-			is_jump = 1'b0;
-			is_reg = 1'b0;
-			is_alu = 1'b0;
+			is_store <= 1'b0;
+			is_load <= 1'b0;
+			is_branch <= 1'b0;
+			is_jump <= 1'b0;
+			is_reg <= 1'b0;
+			is_alu <= 1'b0;
 
-			operand_a = 0;
-			operand_b = 0;
-			branch_dest = 0;
-			dest = 0;
-			func3 = 0;
-			func7 = 0;
+			operand_a <= 0;
+			operand_b <= 0;
+			branch_dest <= 0;
+			dest <= 0;
+			func3 <= 0;
+			func7 <= 0;
 
-			raddr1 = 0;
-			raddr2 = 0;
+			raddr1 <= 0;
+			raddr2 <= 0;
 		end else begin
-			is_store = 1'b0;
-			is_load = 1'b0;
+			is_store <= 1'b0;
+			is_load <= 1'b0;
 
-			is_branch = 1'b0;
-			is_jump = 1'b0;
-			is_reg = 1'b0;
+			is_branch <= 1'b0;
+			is_jump <= 1'b0;
+			is_reg <= 1'b0;
 
-			is_alu = 1'b0;
+			is_alu <= 1'b0;
 
 			case (instr[6:0])
 				/* R-type: register-register */
 				7'b0110011: begin
-					operand_a = rs1;
-					operand_b = rs2;
-					is_alu = 1'b1;
+					operand_a <= rs1;
+					operand_b <= rs2;
+					is_alu <= 1'b1;
 				end
 				/* I-type: short immediates, loads */
 				7'b1100111 || 7'b0000011 || 7'b0010011 || 7'b0001111 || 7'b1110011: begin
-					operand_a = rs1;
-					operand_b = {{20{instr[31]}}, instr[31:20]};
+					operand_a <= rs1;
+					operand_b <= {{20{instr[31]}}, instr[31:20]};
 					case (instr[6:0])
 						7'b0000011: begin
-							is_load = 1'b1;
+							is_load <= 1'b1;
 						end
 						/* jalr */
 						7'b1100111: begin
-							is_jump = 1'b1;
-							is_reg = 1'b1;
+							is_jump <= 1'b1;
+							is_reg <= 1'b1;
 						end
 						7'b0010011: begin
-							is_alu = 1'b1;
+							is_alu <= 1'b1;
 							/* is shift operation? operand_b = shamt */
 							if (func3 == 3'b001 || func3 == 3'b101) begin
-								operand_b = instr[24:20];
+								operand_b <= instr[24:20];
 							end
 						end
 					endcase
 				end
 				/* S-type: stores */
 				7'b0100011: begin
-					is_store = 1'b1;
-					operand_a = instr[19:15] + {{20{instr[31]}}, instr[31:25], instr[11:7]};
-					operand_b = rs2;
+					is_store <= 1'b1;
+					operand_a <= instr[19:15] + {{20{instr[31]}}, instr[31:25], instr[11:7]};
+					operand_b <= rs2;
 				end
 				/* B-type: conditional branches */
 				7'b1100011: begin
-					operand_a = rs1;
-					operand_b = rs2;
-					is_branch = 1'b1;
+					operand_a <= rs1;
+					operand_b <= rs2;
+					is_branch <= 1'b1;
 				end
 				/* U-type: long immediates */
 				7'b0110111 || 7'b0010111: begin
-					operand_a = {instr[31:12], 12'b000000000000};
-					is_load = 1'b1;
+					operand_a <= {instr[31:12], 12'b000000000000};
+					is_load <= 1'b1;
 				end
 				/* J-type: unconditional jumps */
 				7'b1101111: begin
-					operand_a = {11'b00000000000, instr[31], instr[19:12], instr[20], instr[30:21]};
-					is_jump = 1'b1;
+					operand_a <= {11'b00000000000, instr[31], instr[19:12], instr[20], instr[30:21]};
+					is_jump <= 1'b1;
 				end
 				default: ; /* unknown opcode */
 			endcase
