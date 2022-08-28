0d66d7aba058bea0d83f5c653bead4abc846cad0
Till Mahlburg <mahlburg@posteo.de>
2022-08-28 13:53:58 +0200

    first test case (reset) works

--- mriscv.srcs/sources_1/new/instr_decode_tb.v
+++ mriscv.srcs/sources_1/new/instr_decode_tb.v
@@ -11,24 +11,23 @@ module instr_decode_tb ();
 
 	wire is_alu;
 
-	wire [31:0] operand_a, wire [31:0] operand_b;
-	reg [31:0] branch_dest;
-	reg [4:0] dest;
-	reg [2:0] func3;
-	reg func7;
+	wire [31:0] operand_a, operand_b;
+	wire [31:0] branch_dest;
+	wire [4:0] dest;
+	wire [2:0] func3;
+	wire func7;
 
-	reg [31:0] rdata1;
-	reg [31:0] rdata2;
+	wire [31:0] rdata1;
+	wire [31:0] rdata2;
 
-	reg [4:0] raddr1;
-	reg [4:0] raddr2;
+	wire [4:0] raddr1;
+	wire [4:0] raddr2;
 
-
-	integer pass_count;
-	integer fail_count;
+	integer pass;
+	integer fail;
 
 	/* adjust according to the number of test cases */
-	localparam test_count = 0;
+	localparam tests = 1;
 
 	reg [31:0] clk_period;
 
@@ -54,33 +53,53 @@ module instr_decode_tb ();
 		.raddr2(raddr2)
 	);
 
+	reg w_en;
+	reg [4:0] waddr;
+	reg [31:0] wdata;
+
 	regs regs (
+		.w_en(w_en),
 		.raddr1(raddr1),
 		.raddr2(raddr2),
+		.waddr(waddr),
 		.rdata1(rdata1),
-		.rdata2(rdata2)
+		.rdata2(rdata2),
+		.wdata(wdata)
 	);
 
 	initial begin
 		$dumpfile("instr_decode_tb.vcd");
 		$dumpvars(0, instr_decode_tb);
 
-		reset = 0;
+		reset = 1;
 		clk = 0;
 		clk_period = 10;
 
-		pass_count = 0;
-		fail_count = 0;
+		pass = 0;
+		fail = 0;
 
-		if ((pass_count + fail_count) == total) begin
-			$display("PASSED: number of test cases");
-			pass_count = pass_count + 1;
+		#10;
+
+		if ((is_store | is_load | is_branch | is_jump | is_reg
+			| is_alu | operand_a | operand_b | branch_dest
+			| dest | func3 | func7 | raddr1 | raddr2) == 0) begin
+			$display("PASSED: reset");
+			pass = pass + 1;
 		end else begin
-			$display("FAILED: number of test cases");
-			fail_count = fail_count + 1;
+			$display("FAILED: reset");
+			fail = fail + 1;
 		end
 
-		$display("%0d/%0d PASSED", pass_count, (test_count + 1));
+
+		if ((pass + fail) == tests) begin
+			$display("PASSED: number of test cases");
+			pass = pass + 1;
+		end else begin
+			$display("FAILED: number of test cases");
+			fail = fail + 1;
+		end
+
+		$display("%0d/%0d PASSED", pass, (tests + 1));
 		$finish;
 	end
 
