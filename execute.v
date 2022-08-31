385b5e95162c6cb56e66a404cd508d061e66f533
Till Mahlburg <mahlburg@posteo.de>
2022-08-31 19:27:27 +0200

    implement reset state

--- execute.v
+++ execute.v
@@ -30,87 +30,93 @@ module execute (
 	reg branch;
 
 	always @(posedge clk) begin
-		dest_o <= dest_i;
-		branch <= 1'b0;
+		if (reset) begin
+			dest_o <= 0;
+			result <= 0;
+			next_pc <= 0;
+		end else begin
+			dest_o <= dest_i;
+			branch <= 1'b0;
 
-		next_pc <= curr_pc + 4;
+			next_pc <= curr_pc + 4;
 
-		case (1'b1)
-			is_store: ; /* TODO */
+			case (1'b1)
+				is_store: ; /* TODO */
 
-			is_load: ; /* TODO */
+				is_load: ; /* TODO */
 
-			is_branch: begin
-				case (func3)
-					/* beq */
-					3'b000: branch <= (operand_a == operand_b);
-					/* bne */
-					3'b001: branch <= (operand_a != operand_b);
-					/* blt */
-					3'b100: branch <= (operand_a < operand_b);
-					/* bge */
-					3'b101: branch <= (operand_a >= operand_b);
-					/* bltu */
-					3'b110: branch <= ($unsigned(operand_a) < $unsigned(operand_b));
-					/* bgeu */
-					3'b111: branch <= ($unsigned(operand_a) >= $unsigned(operand_b));
-				endcase
-				if (branch) begin
-					next_pc <= curr_pc + branch_dest;
+				is_branch: begin
+					case (func3)
+						/* beq */
+						3'b000: branch <= (operand_a == operand_b);
+						/* bne */
+						3'b001: branch <= (operand_a != operand_b);
+						/* blt */
+						3'b100: branch <= (operand_a < operand_b);
+						/* bge */
+						3'b101: branch <= (operand_a >= operand_b);
+						/* bltu */
+						3'b110: branch <= ($unsigned(operand_a) < $unsigned(operand_b));
+						/* bgeu */
+						3'b111: branch <= ($unsigned(operand_a) >= $unsigned(operand_b));
+					endcase
+					if (branch) begin
+						next_pc <= curr_pc + branch_dest;
+					end
 				end
-			end
-			is_jump: begin
-				result = curr_pc + 4;
-				/* when there is no destination specified, register x1 is assumed */
-				if (dest_i == 5'b00000) begin
-					dest_o <= 5'b00001;
+				is_jump: begin
+					result = curr_pc + 4;
+					/* when there is no destination specified, register x1 is assumed */
+					if (dest_i == 5'b00000) begin
+						dest_o <= 5'b00001;
+					end
+					if (is_reg) begin
+						next_pc <= (operand_a + operand_b) & ~1;
+					end else begin
+						next_pc <= curr_pc + operand_a;
+					end
 				end
-				if (is_reg) begin
-					next_pc <= (operand_a + operand_b) & ~1;
-				end else begin
-					next_pc <= curr_pc + operand_a;
-				end
-			end
 
-			is_alu: begin
-				case (func3)
-					/* add */
-					3'b000: begin
-						if (func7 === 1'b0) begin
-							result <= operand_a + operand_b;
-						end else begin
-							result <= operand_a - operand_b;
+				is_alu: begin
+					case (func3)
+						/* add */
+						3'b000: begin
+							if (func7 === 1'b0) begin
+								result <= operand_a + operand_b;
+							end else begin
+								result <= operand_a - operand_b;
+							end
 						end
-					end
-					/* and */
-					3'b111: begin
-						result <= operand_a & operand_b;
-					end
-					/* or */
-					3'b110: begin
-						result <= operand_a | operand_b;
-					end
-					/* xor */
-					3'b100: begin
-						result <= operand_a ^ operand_b;
-					end
-					/* left shift */
-					3'b001: begin
-						result <= operand_a << operand_b[4:0];
-					end
-					/* right shift */
-					3'b101: begin
-						/* logical */
-						if (func7 === 1'b0) begin
-							result <= operand_a >> operand_b[4:0];
-						/* arithmetic */
-						end else begin
-							result <= operand_a >>> operand_b[4:0];
+						/* and */
+						3'b111: begin
+							result <= operand_a & operand_b;
 						end
-					end
-				endcase
-			end
-		endcase
+						/* or */
+						3'b110: begin
+							result <= operand_a | operand_b;
+						end
+						/* xor */
+						3'b100: begin
+							result <= operand_a ^ operand_b;
+						end
+						/* left shift */
+						3'b001: begin
+							result <= operand_a << operand_b[4:0];
+						end
+						/* right shift */
+						3'b101: begin
+							/* logical */
+							if (func7 === 1'b0) begin
+								result <= operand_a >> operand_b[4:0];
+							/* arithmetic */
+							end else begin
+								result <= operand_a >>> operand_b[4:0];
+							end
+						end
+					endcase
+				end
+			endcase
+		end
 	end
 
 endmodule
