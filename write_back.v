5b40e7d9c8dea794979aa5acce57f6222ec7a64b
Till Mahlburg <mahlburg@posteo.de>
2022-09-23 14:03:04 +0200

    fix some minor issues

--- write_back.v
+++ write_back.v
@@ -1,21 +1,22 @@
 `timescale 1 ns / 1 ps
 
 module write_back #(
-	parameter [31:0] STACK_ADDR = 0;
+	parameter [31:0] STACK_ADDR = 0
 ) (
 	input clk,
 	input reset,
 
 	input [31:0] result,
-	input [5:0] dest,
+	input [4:0] dest,
 	input [31:0] next_pc,
 
 	output reg [31:0] pc,
 
 	output reg w_en,
 	output reg [31:0] wdata,
-	output reg [5:0] waddr
+	output reg [4:0] waddr
 );
+
 	always @(posedge clk) begin
 		if (reset) begin
 			pc <= STACK_ADDR;
@@ -23,16 +24,11 @@ module write_back #(
 			wdata <= 0;
 			waddr <= 0;
 		end else begin
+			w_en <= 1'b1;
 			wdata <= result;
 			waddr <= dest;
-			w_en <= 1'b1;
 			pc <= next_pc;
 		end
 	end
 
-	always @(negedge clk) begin
-		if (!reset) begin
-			w_en <= 1'b0;
-		end
-	end
-end module
+endmodule
